8669948b8f029aeee159156f0f1e4109b6f9df7c
Alperen <eren.sertkaya.es@gmail.com>
2023-11-15 16:42:05 +0300

    feat: add new commands

--- src/App.js
+++ src/App.js
@@ -8,28 +8,64 @@ function App() {
 
     const handleInput = (event) => {
         if (event.key === 'Enter') {
-            const command = event.target.value;
+            const command = event.target.value.split(" ");
             event.target.value = '';
-            setOutput([...output, `$ ${command}`]);
-            setInputArr([...inputArr, `${command}`]);
+            setOutput([...output, `$ ${command.length > 1 ? command[0] + command[1] : command}`]);
+            setInputArr([...inputArr, `${command.length > 1 ? command[0]+' '+command[1] : command}`]);
             executeCommand(command);
+            setInput('');
         }
     };
 
     const executeCommand = (command) => {
         // Handle specific commands
-        switch (command.toLowerCase()) {
+        switch (command[0]) {
             case 'help':
                 showHelp();
+                break;
+            case 'cat':
+                showCat(command[1]);
+                break;
+            case 'ls':
+                showLs();
                 break;
             default:
                 setOutput([...output, `${command} is not recognized as a command`]);
                 break;
         }
     };
-
+    function replaceWithBr(text) {
+        return text.replace(/\n/g, "<br />")
+    }
     const showHelp = () => {
-        setOutput([...output, 'Available commands: help - Show this help message']);
+        setOutput([...output,
+            `Available commands:
+
+            help  - Show this help message
+            cat   - Show file
+            ls    - List files
+            clear - Clear all commands
+             `]);
+    };
+
+    const showCat = (param) => {
+        switch (param) {
+            case 'cv.txt':
+                setOutput([...output, 'benim cv bu']);
+                break;
+            default:
+                setOutput([...output, 'Nothing to show.']);
+                break;
+        }
+    };
+
+    const showLs = () => {
+        setOutput([...output,
+            `cv.txt
+             projects.txt
+             skills.txt
+             contact.txt
+             `]);
     };
 
     return (
@@ -41,7 +77,7 @@ function App() {
                             <span key={index}>[master@fedora ~]$ {line}</span>
                             {output.map((outputLine, outputIndex) => (
                                 index === outputIndex ?
-                                    <p key={outputIndex}>{outputLine}</p> :
+                                    <p key={outputIndex} dangerouslySetInnerHTML={{__html: replaceWithBr(outputLine)}} ></p> :
                                     ''
                             ))}
                         </div>
